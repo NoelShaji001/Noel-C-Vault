ddc87a207583d2e88bd0b64aab7eb47706bee516
NoelShaji001 <noelshaji257202@gmail.com>
2025-02-12 19:26:53 +0530

    Update sequential_search.c

--- sequential_search.c
+++ sequential_search.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+include<stdio.h>
 int main(){
 	int i,n,key,pos=0,flag=0;
 	printf("Enter the number of values:");
@@ -11,16 +11,17 @@ int main(){
 	printf("Enter the key:");
 	scanf("%d",&key);
 	for(i=0;i<n;i++){
-		if(a[i]==key){
+		if(key==a[i]){
 			flag=1;
 			pos=i;
-		}
+		}else{
+            flag=0;
+        }    
 	}
-	if(flag=1){
-		printf("The number found and its position is %d",pos);
-	}else{
+	if(flag==0){
 		printf("The number not found");
+	}else{
+		printf("The number found and its position is %d",pos);
 	}
 	return 0;
 }
-			
